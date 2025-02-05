a2d2942b355bff8cec7056828bb2c05474fa4de9
yeazzing <yazin71@gmail.com>
2025-02-05 13:46:10 +0000

    train code update

--- ViT/vit_training.py
+++ ViT/vit_training.py
@@ -21,13 +21,15 @@ def main():
     parser = argparse.ArgumentParser(description="Train params")
     
     parser.add_argument("--data_path", type=Path, default=Path(f"{ROOT}/data"))
+    parser.add_argument("--model_path", type=str, default='google/vit-base-patch16-224')
     parser.add_argument("--save_path", type=Path, default=Path(f"{ROOT}/results"))
     parser.add_argument("--batch_size", type=int, default=16)
-    parser.add_argument("--lr", type=float, default=0.01)
-    parser.add_argument("--epochs", type=int, default=10)
+    parser.add_argument("--lr", type=float, default=1e-5)
+    parser.add_argument("--epochs", type=int, default=30)
     
     
     args = parser.parse_args()
+    wandb.config.update(args)
     
     transform_train=albumentations.Compose([
             #albumentations.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
@@ -41,11 +43,11 @@ def main():
     
     id2label = get_id2label(args.data_path, "train")
 
-    train_dataloader = data.DataLoader(dataset=train_data, batch_size=args.batch_size, num_workers=0, shuffle=True)
+    train_dataloader = data.DataLoader(dataset=train_data, batch_size=args.batch_size, num_workers=4, shuffle=True)
     val_dataloader = data.DataLoader(dataset=val_data, batch_size=args.batch_size, num_workers=0, shuffle=False)
     
     model = ViTForImageClassification.from_pretrained(
-        pretrained_model_name_or_path='google/vit-base-patch16-224', 
+        pretrained_model_name_or_path=args.model_path, 
         id2label=id2label, 
         num_labels=len(id2label),
         ignore_mismatched_sizes=True
@@ -54,7 +56,7 @@ def main():
     model_parameter = model.parameters()
     
     optimizer = optim.Adam(model_parameter, lr=args.lr)
-    scheduler = optim.lr_scheduler.LinearLR(optimizer)
+    scheduler = optim.lr_scheduler.LinearLR(optimizer, total_iters=20)
     
     loss_fn = torch.nn.CrossEntropyLoss()
     
@@ -66,7 +68,7 @@ def main():
     for epoch in range(epochs):
         model.train()
         total_loss, total_val_loss = 0, 0
-        for step, batch in enumerate(tqdm(train_dataloader)):
+        for batch in tqdm(train_dataloader):
             
             image = batch['image'].to(device)
             label = batch['label'].to(device)
@@ -80,12 +82,14 @@ def main():
             optimizer.step()
             scheduler.step()
             optimizer.zero_grad()
-            wandb.log({"train_loss": loss})
+            
+            #print every batches
+            wandb.log({"train_loss": loss, "lr": scheduler.get_last_lr()[0]})
             
         
         model.eval()
         pred_list, label_list = [], []
-        for val_step, batch in enumerate(tqdm(val_dataloader)):
+        for batch in tqdm(val_dataloader):
             image = batch['image'].to(device)
             label = batch['label'].to(device)
             with torch.no_grad():
@@ -120,9 +124,10 @@ def main():
         if best_score < score['accuracy']:
             now = datetime.now(pytz.timezone('Asia/Seoul')).strftime('%Y%m%d-%H:%M:%S')
             Path(f"{args.save_path}/{now}").mkdir(parents=True, exist_ok=True)
-            target_path = Path(f"{args.save_path}/{now}/weight_{epoch}.pt")
-            torch.save(model.state_dict(), target_path)
-            torch.save(optimizer.state_dict(), target_path)
+            moodel_path = Path(f"{args.save_path}/{now}/weight_{epoch}.pt")
+            optim_path = Path(f"{args.save_path}/{now}/optim_{epoch}.pt")
+            torch.save(model.state_dict(), moodel_path)
+            torch.save(optimizer.state_dict(), optim_path)
             best_score = score['accuracy']
         
         print(f" Epoch ({epoch}), Best accuracy: {best_score}")   
